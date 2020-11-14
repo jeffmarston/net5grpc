5e00994b2375088a7d7a0fb6cb743f86db73f14c
Jeff Marston <jmarston@ezesoft.com>
2020-11-13 20:40:33 -0500

    first commit

---
+++ net5Library/Communicator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace net5Library
+{
+    public class Communicator
+    {
+        public string GetStatus(int number)
+        {
+            return $"Pending {number} tasks.";
+        }
+    }
+}
